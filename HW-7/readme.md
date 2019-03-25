e7d098d0ff6024a9a7ab4d8a17aae46ec8910b91
Zoie <zkoostra@gmail.com>
2019-03-25 17:07:47 -0600

    hw8

---
+++ HW-7/readme.md
@@ -0,0 +1,9 @@
+# Homework 7 readme
+
+Here we are orienting our programming around objects! Last year, I was doing MART 120 at the same time as like CS100 or something similar and I did a javascript project in my cs class in oop just to get better at it and whoa boy I wish I hadn't forgotten like every single thing I did last spring!!
+
+I think the idea of OOP is super amazing and once you really start actively thinking in it, it's really helpful and makes things a lot easier. But I had a hard time getting back into the syntax of it and obviously p5 and the projects we did in 120 in general were formatted differently and had really different requirements from the assignment we did this week.
+
+I work at the Kaimin with 2 other girls who are in this class so I wound up consulting them on their techniques for this assignment a little bit but that made it so I felt like I wasn't really coming up with the whole ideas myself, and therefore I don't grasp them quite as well?
+
+I'm turning this in not working totally perfectly. I feel so dumb because my images won't appear even though I've checked my file names and paths like a million times and for some reason that I also can't figure out, my getInfo() function breaks after it runs the first time so you can only see one of the photos. Not a super week for me, but I'm still going along. I'm completing this after the fact and I can say hopefully that the assignments that followed this one seem to be going somewhat better haha.
--- HW-8/scripts/script.js
+++ HW-8/scripts/script.js
@@ -1,56 +1,94 @@
-let pineappleSelector = "#pineapple1";
-let pineApples = new Array();
-class PineappleInfo{
-    constructor(selector, imagePath)
-    {
-        this.selector = selector;
-        this.imagePath = imagePath;
-    }
-
-    get theSelector(){
-        return this.selector;
-    }
-
-    get theImagePath(){
-        return this.imagePath;
-    }
-
-    toString()
-    {
-        return this.selector + ":" + this.imagePath;
-    }
+let pineappleArray = new Array();
+let textArray = new Array();
+let boxArray = new Array();
+class Images {
+  constructor(w, h, path) {
+    this.w = w;
+    this.h = h;
+    this.path = path;
+  }
+  get width() {
+    return this.w;
+  }
+  get height() {
+    return this.h;
+  }
+  get thePath() {
+    return this.path;
+  }
 }
 
-function initializeArray()
-{
-    var pineapple = new PineappleInfo("#pineapple1", "images/pineapple-1.jpg");
-    pineApples.push(pineapple1);
-
+class Text {
+  constructor(message) {
+    this.message = message;
+  }
+  get messages() {
+    return this.message;
+  }
 }
-$(document).ready(function(){
-    console.log(pineApples[0].toString());
-    console.log(pineApples[0].theSelector);
-    console.log(pineApples[0].theImagePath);
 
-    //$(allDogs[0].theSelector).src = allDogs[0].theImagePath;
-    //$("#my_image").attr("src","second.jpg");
-    $(pineApples[0].theSelector).attr("src", pineApples[0].theImagePath);
-
-
-          setInterval($(pineApples[0].theSelector).fadeOut().fadeIn();, 1000);
-
-       //$(pineApples[0].theSelector).fadeOut().fadeIn();
-
-       showText(){
-         $(plainText).fadeIn
-       }
-
-});
-
-
-
-
-// function moveSquare()
-// {
-//     $("#square").animate({left:250}).animate({top:400}).animate({left:0}).animate({top:300});
+// class Box {
+//   constructor(w, h, fill) {
+//     this.w = w;
+//     this.h = h;
+//     this.fill = fill;
+//   }
+//
+//   get boxes(){
+//     return this.box;
+//   }
 // }
+
+//adding pineapple images to the pinappleArray
+function addPineapples() {
+  let pineappleImage1 = new Images(400, 400, "images/pineapple-1.jpg");
+  let pineappleImage2 = new Images(400, 400, "images/pineapple-2.jpg");
+  pineappleArray.push(pineappleImage1);
+  pineappleArray.push(pineappleImage2);
+}
+
+//adding text to the text array
+function addText() {
+  let message1 = new Text("Are you a fruit?");
+  let message2 = new Text("Because baby, you look like a fineapple!");
+
+  textArray.push(message1);
+  textArray.push(message2);
+
+}
+
+
+
+
+//connect the image path as shown in the array w the jquery object that is the pineapple image
+$(document).ready(function() {
+  addPineapples();
+  addText();
+  $('#pineapple1').attr("src", pineappleArray[0].thePath);
+  $('.plainText').text(textArray[0].messages);
+
+  //when the image is clicked, fade out first image and replace with the second one.
+  $("#pineapple1").click(function() {
+    $("#pineapple1").fadeOut(1000, changeImage);
+     $('.plainText').fadeOut(1000, changeText);
+     $('#box1').animate({left:"+50"}), 1000;
+  });
+
+  //function that resets the path of the pineapple image 1 jquery object so it corresponds with the  actual image and path for the second pineapple photo, then fades it back in.
+  function changeImage() {
+    $('#pineapple1').attr("src", pineappleArray[1].thePath);
+    $("#pineapple1").fadeIn(1000);
+
+  }
+
+//Replace Old, Boring text with Fun compliment text!! It seems like it's appearing really fast ie not fading in in one second but idk why that is.
+  function changeText(){
+    $('.coolText').text(textArray[1].messages);
+    $('.coolText').fadeIn(1000);
+  }
+
+//
+  function moveFirstBox(){
+  $("#box1").animate({left:250}).animate({left:800});
+    }
+});
