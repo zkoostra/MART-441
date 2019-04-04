e8b8a4492c0f6a330f19149dde39473e2c3f8048
Zoie <zkoostra@gmail.com>
2019-04-03 20:51:43 -0600

    fixing stuff!!

--- HW-910/scripts/json_ajax_space.js
+++ HW-910/scripts/json_ajax_space.js
@@ -1,34 +1,49 @@
 
 //I wound up trying two super different thinks to try and think out this problem so this is way #1 but LOL it does not work.
-class People {
+class Person {
   constructor(craft, name, gender){
     this.craft = craft
     this.name = name
     this.gender = gender
   }
 
-  get craft(){
-    //so this is where I link the data and the class but I'm not sure how
+  get station(){
+    return this.craft
   }
-  get name(){
-    //so this is where I link the data and the class but I'm not sure how
-  }
-
-  get gender(){
-    //so this is where I link the data and the class but I'm not sure how
+  get person(){
+    return this.name
   }
 
-  $(document).ready(function(){
-  //I'm assuming this should be inside the constructor but I don't know if that's right or if I even need to do this if I have something to get the data in the get functions
-  $("button").click(function(){
-  $.getJSON("data/ppl_in_space.json", function(result){
-  $.each(result, function(i, field){
-  $("div").append(field + " ");
-  });
-  });
-  });
-  });
-
+  get theGender(){
+    return this.gender
+  }
 
 
 }
+
+let peopleArray = []
+
+
+$(document).ready(function(){  window.alert("test")
+//I'm assuming this should be inside the constructor but I don't know if that's right or if I even need to do this if I have something to get the data in the get functions
+$("button").click(function(){
+    window.alert("test")
+$.getJSON("data/ppl_in_space.json", function(result){
+    window.alert("test")
+//$.each(result, function(makePeople){
+  for (var i = 0; i<result.people.length; i++){
+    peopleArray.push(new Person(result.people[i].craft,result.people[i].name, result.people[i].gender))
+      window.alert("test")
+  }
+
+var theFinalStuff = "<table>"
+for (var i =0; i<peopleArray.length; i++){
+  theFinalStuff = theFinalStuff+"<tr><td>"+peopleArray[i].person +"</td> <td>"+peopleArray[i].station + "</td> <td>" + peopleArray[i].theGender +"</td></tr>"
+
+}
+theFinalStuff = theFinalStuff +"</table>"
+
+$("div").html(theFinalStuff)
+});
+});
+});
